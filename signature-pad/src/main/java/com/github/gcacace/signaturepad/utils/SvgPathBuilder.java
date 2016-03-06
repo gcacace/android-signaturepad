7ee3a14b724721c4ccd0b772a5917a3611f82e7a
Thomas LEVEIL <thomasleveil@gmail.com>
2016-03-07 00:04:38 +0100

    SVG optimize: use relative coordinates

--- signature-pad/src/main/java/com/github/gcacace/signaturepad/utils/SvgPathBuilder.java
+++ signature-pad/src/main/java/com/github/gcacace/signaturepad/utils/SvgPathBuilder.java
@@ -1,15 +1,14 @@
 package com.github.gcacace.signaturepad.utils;
 
 
-
 /**
  * Build a SVG path as a string.
- *
+ * <p/>
  * https://www.w3.org/TR/SVGTiny12/paths.html
  */
 public class SvgPathBuilder {
 
-    public static final Character SVG_ABSOLUTE_CUBIC_BEZIER_CURVE = 'C';
+    public static final Character SVG_RELATIVE_CUBIC_BEZIER_CURVE = 'c';
     public static final Character SVG_MOVE = 'M';
     private final StringBuilder mStringBuilder;
     private final Integer mStrokeWidth;
@@ -34,13 +33,8 @@ public class SvgPathBuilder {
     }
 
     public SvgPathBuilder append(final SvgPoint controlPoint1, final SvgPoint controlPoint2, final SvgPoint endPoint) {
-        if (SVG_ABSOLUTE_CUBIC_BEZIER_CURVE.equals(mLastSvgCommand)) {
-            mStringBuilder.append(' ');
-        } else {
-            mStringBuilder.append(SVG_ABSOLUTE_CUBIC_BEZIER_CURVE);
-        }
-        mStringBuilder.append(makeAbsoluteCubicBezierCurve(controlPoint1, controlPoint2, endPoint));
-        mLastSvgCommand = SVG_ABSOLUTE_CUBIC_BEZIER_CURVE;
+        mStringBuilder.append(makeRelativeCubicBezierCurve(controlPoint1, controlPoint2, endPoint));
+        mLastSvgCommand = SVG_RELATIVE_CUBIC_BEZIER_CURVE;
         mLastPoint = endPoint;
         return this;
     }
@@ -60,13 +54,38 @@ public class SvgPathBuilder {
                 .toString();
     }
 
-    private String makeAbsoluteCubicBezierCurve(final SvgPoint controlPoint1, final SvgPoint controlPoint2, final SvgPoint endPoint) {
-        return (new StringBuilder())
-                .append(controlPoint1)
-                .append(" ")
-                .append(controlPoint2)
-                .append(" ")
-                .append(endPoint)
-                .toString();
+    private String makeRelativeCubicBezierCurve(final SvgPoint controlPoint1, final SvgPoint controlPoint2, final SvgPoint endPoint) {
+        final String sControlPoint1 = controlPoint1.toRelativeCoordinates(mLastPoint);
+        final String sControlPoint2 = controlPoint2.toRelativeCoordinates(mLastPoint);
+        final String sEndPoint = endPoint.toRelativeCoordinates(mLastPoint);
+
+        final StringBuilder sb = new StringBuilder();
+        if (!SVG_RELATIVE_CUBIC_BEZIER_CURVE.equals(mLastSvgCommand)) {
+            sb.append(SVG_RELATIVE_CUBIC_BEZIER_CURVE);
+            sb.append(sControlPoint1);
+        } else {
+            if (!sControlPoint1.startsWith("-")) {
+                sb.append(" ");
+            }
+            sb.append(sControlPoint1);
+        }
+
+        if (!sControlPoint2.startsWith("-")) {
+            sb.append(" ");
+        }
+        sb.append(sControlPoint2);
+
+        if (!sEndPoint.startsWith("-")) {
+            sb.append(" ");
+        }
+        sb.append(sEndPoint);
+
+        // discard zero curve
+        final String svg = sb.toString();
+        if ("c0 0 0 0 0 0".equals(svg)) {
+            return "";
+        } else {
+            return svg;
+        }
     }
 }
