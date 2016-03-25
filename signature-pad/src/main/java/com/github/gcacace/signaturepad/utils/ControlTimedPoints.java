f537079d71e71f0bbc51981750b6f28dfaef3f34
Eyal Biran <eyal@everything.me>
2016-03-25 20:42:22 +0300

    Improve performance by recycling objects (especially needed for onTouch rapid events)

--- signature-pad/src/main/java/com/github/gcacace/signaturepad/utils/ControlTimedPoints.java
+++ signature-pad/src/main/java/com/github/gcacace/signaturepad/utils/ControlTimedPoints.java
@@ -8,9 +8,10 @@ public class ControlTimedPoints {
     public TimedPoint c1;
     public TimedPoint c2;
 
-    public ControlTimedPoints(TimedPoint c1, TimedPoint c2) {
+    public ControlTimedPoints set(TimedPoint c1, TimedPoint c2) {
         this.c1 = c1;
         this.c2 = c2;
+        return this;
     }
 
 }
--- signature-pad/src/main/java/com/github/gcacace/signaturepad/views/SignaturePad.java
+++ signature-pad/src/main/java/com/github/gcacace/signaturepad/views/SignaturePad.java
@@ -36,6 +36,11 @@ public class SignaturePad extends View {
 
     private final SvgBuilder mSvgBuilder = new SvgBuilder();
 
+    // Cache
+    private List<TimedPoint> mPointsCache = new ArrayList<>();
+    private ControlTimedPoints mControlTimedPointsCached = new ControlTimedPoints();
+    private Bezier mBezierCached = new Bezier();
+
     //Configurable parameters
     private int mMinWidth;
     private int mMaxWidth;
@@ -159,17 +164,17 @@ public class SignaturePad extends View {
                 mPath.moveTo(eventX, eventY);
                 mLastTouchX = eventX;
                 mLastTouchY = eventY;
-                addPoint(new TimedPoint(eventX, eventY));
+                addPoint(getNewPoint(eventX, eventY));
                 if(mOnSignedListener != null) mOnSignedListener.onStartSigning();
 
             case MotionEvent.ACTION_MOVE:
                 resetDirtyRect(eventX, eventY);
-                addPoint(new TimedPoint(eventX, eventY));
+                addPoint(getNewPoint(eventX, eventY));
                 break;
 
             case MotionEvent.ACTION_UP:
                 resetDirtyRect(eventX, eventY);
-                addPoint(new TimedPoint(eventX, eventY));
+                addPoint(getNewPoint(eventX, eventY));
                 getParent().requestDisallowInterceptTouchEvent(true);
                 setIsEmpty(false);
                 break;
@@ -332,18 +337,39 @@ public class SignaturePad extends View {
       return Bitmap.createBitmap(mSignatureBitmap, xMin, yMin, xMax - xMin, yMax - yMin);
     }
 
+    private TimedPoint getNewPoint(float x, float y) {
+        int mCacheSize = mPointsCache.size();
+        TimedPoint timedPoint;
+        if (mCacheSize == 0) {
+            // Cache is empty, create a new point
+            timedPoint = new TimedPoint();
+        } else {
+            // Get point from cache
+            timedPoint = mPointsCache.remove(mCacheSize-1);
+        }
+
+        return timedPoint.set(x, y);
+    }
+
+    private void recyclePoint(TimedPoint point) {
+        mPointsCache.add(point);
+    }
+
     private void addPoint(TimedPoint newPoint) {
         mPoints.add(newPoint);
-        if (mPoints.size() > 2) {
-            // To reduce the initial lag make it work with 3 mPoints
-            // by copying the first point to the beginning.
-            if (mPoints.size() == 3) mPoints.add(0, mPoints.get(0));
+
+        int pointsCount = mPoints.size();
+        if (pointsCount > 3) {
 
             ControlTimedPoints tmp = calculateCurveControlPoints(mPoints.get(0), mPoints.get(1), mPoints.get(2));
             TimedPoint c2 = tmp.c2;
+            recyclePoint(tmp.c1);
+
             tmp = calculateCurveControlPoints(mPoints.get(1), mPoints.get(2), mPoints.get(3));
             TimedPoint c3 = tmp.c1;
-            Bezier curve = new Bezier(mPoints.get(1), c2, c3, mPoints.get(2));
+            recyclePoint(tmp.c2);
+
+            Bezier curve = mBezierCached.set(mPoints.get(1), c2, c3, mPoints.get(2));
 
             TimedPoint startPoint = curve.startPoint;
             TimedPoint endPoint = curve.endPoint;
@@ -369,7 +395,16 @@ public class SignaturePad extends View {
 
             // Remove the first element from the list,
             // so that we always have no more than 4 mPoints in mPoints array.
-            mPoints.remove(0);
+            recyclePoint(mPoints.remove(0));
+
+            recyclePoint(c2);
+            recyclePoint(c3);
+            
+        } else if (pointsCount == 1) {
+            // To reduce the initial lag make it work with 3 mPoints
+            // by duplicating the first point
+            TimedPoint firstPoint = mPoints.get(0);
+            mPoints.add(getNewPoint(firstPoint.x, firstPoint.y));
         }
     }
 
@@ -414,22 +449,25 @@ public class SignaturePad extends View {
         float dx2 = s2.x - s3.x;
         float dy2 = s2.y - s3.y;
 
-        TimedPoint m1 = new TimedPoint((s1.x + s2.x) / 2.0f, (s1.y + s2.y) / 2.0f);
-        TimedPoint m2 = new TimedPoint((s2.x + s3.x) / 2.0f, (s2.y + s3.y) / 2.0f);
+        float m1X = (s1.x + s2.x) / 2.0f;
+        float m1Y = (s1.y + s2.y) / 2.0f;
+        float m2X = (s2.x + s3.x) / 2.0f;
+        float m2Y = (s2.y + s3.y) / 2.0f;
 
         float l1 = (float) Math.sqrt(dx1 * dx1 + dy1 * dy1);
         float l2 = (float) Math.sqrt(dx2 * dx2 + dy2 * dy2);
 
-        float dxm = (m1.x - m2.x);
-        float dym = (m1.y - m2.y);
+        float dxm = (m1X - m2X);
+        float dym = (m1Y - m2Y);
         float k = l2 / (l1 + l2);
         if (Float.isNaN(k)) k = 0.0f;
-        TimedPoint cm = new TimedPoint(m2.x + dxm * k, m2.y + dym * k);
+        float cmX = m2X + dxm * k;
+        float cmY = m2Y + dym * k;
 
-        float tx = s2.x - cm.x;
-        float ty = s2.y - cm.y;
+        float tx = s2.x - cmX;
+        float ty = s2.y - cmY;
 
-        return new ControlTimedPoints(new TimedPoint(m1.x + tx, m1.y + ty), new TimedPoint(m2.x + tx, m2.y + ty));
+        return mControlTimedPointsCached.set(getNewPoint(m1X + tx, m1Y + ty), getNewPoint(m2X + tx, m2Y + ty));
     }
 
     private float strokeWidth(float velocity) {
